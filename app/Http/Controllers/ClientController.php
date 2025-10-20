f92afefb44ab8bc549432b213784a214687ccce7
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-20 10:47:34 -0300

    sobre rotas automáticas

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,45 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
-        //
+        //Mostrando todos os clientes 
+        //aqui eu faço um select de todos os clientes 
+        //e depois faço um conversão para json e retorno o status 200
+        //em uma linha so 
+        
+        return response()->json(Client::all(), 200);
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+ 
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+   
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+   
     public function destroy(string $id)
     {
         //
