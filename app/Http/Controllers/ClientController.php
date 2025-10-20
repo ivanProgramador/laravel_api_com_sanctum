c89a0963171d2aa4f1a90247362eb86090bcd83d
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-20 11:07:14 -0300

    requisicao para gravar os dados dos clientes

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -22,7 +22,28 @@ class ClientController extends Controller
  
     public function store(Request $request)
     {
-        //
+         //no caso desse metodo a rota e a mesma so muda o verbo http
+        //aqui eu vou cadastrar um novo cliente 
+        //então o verbo é post 
+
+        //validando a requisição 
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:clients',
+            'phone' => 'required',
+        ]);
+
+        //criando um novo cliente
+        //essa é uma forma mais rapida de criar um cliente
+        //usando o método create do model Client
+        //ele recebe um array com os dados do cliente
+
+        $client = Client::create($request->all());
+
+        return response()->json([
+            'message' => 'cliente cadastrado com sucesso',
+            'data' => $client
+        ]);
     }
 
    
