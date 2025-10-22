262ae9505779ed55583a819114c674b0c5ec0031
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-21 22:13:32 -0300

    rotas  protegidas com sanctum

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,12 @@ use Illuminate\Http\Request;
 
 Route::get('/status',function(){
     return ApiResponse::success('Api funcionando');
-});
+})->middleware('auth:sanctum');
 
 //so o fato de digitar essa linha ja cria todas as rotas para o controlador ClientController
 //baseado nos metodos que ele ja possui
 
-Route::apiResource('clients', ClientController::class);
+Route::apiResource('clients', ClientController::class)->middleware('auth:sanctum');
 
 //rota para login
 Route::post('/login', [AuthController::class, 'login']);
