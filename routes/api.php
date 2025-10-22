4587caaedea59852aea9c3813b2c5b5174a69174
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-22 15:59:01 -0300

    autenticacao com abilites removida das rotas pra evitar o ponto de atencao

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,4 @@ Route::post('/login', [AuthController::class, 'login']);
 //rota para logout
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-//para poder testar o token abilities eu preciso trazer as rotas associdas pra esse arquivo 
-
-Route::get('/clients',[ClientController::class,'index'])->middleware(['auth:sanctum','ability:clients:list']);
-Route::post('/clients',[ClientController::class,'store'])->middleware(['auth:sanctum','ability:clients:create']);
-Route::get('/clients/{client}',[ClientController::class,'show'])->middleware(['auth:sanctum','ability:clients:view']);
-Route::put('/clients/{client}',[ClientController::class,'update'])->middleware(['auth:sanctum','ability:clients:update']);
-Route::delete('/clients/{client}',[ClientController::class,'destroy'])->middleware(['auth:sanctum','ability:clients:delete']);
-
 
