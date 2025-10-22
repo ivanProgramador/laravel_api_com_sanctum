1c36775cfabfe7bbbc8cb25d46f12498d83cffa2
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-22 12:04:01 -0300

    usando o token abilities para proteger as rotas de forma direta

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\AuthController;
+use App\Models\Client;
 use App\services\ApiResponse;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -20,3 +21,13 @@ Route::post('/login', [AuthController::class, 'login']);
 
 //rota para logout
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//para poder testar o token abilities eu preciso trazer as rotas associdas pra esse arquivo 
+
+Route::get('/clients',[ClientController::class,'index'])->middleware(['auth:sanctum','ability:clients:list']);
+Route::post('/clients',[ClientController::class,'store'])->middleware(['auth:sanctum','ability:clients:create']);
+Route::get('/clients/{client}',[ClientController::class,'show'])->middleware(['auth:sanctum','ability:clients:view']);
+Route::put('/clients/{client}',[ClientController::class,'update'])->middleware(['auth:sanctum','ability:clients:update']);
+Route::delete('/clients/{client}',[ClientController::class,'destroy'])->middleware(['auth:sanctum','ability:clients:delete']);
+
+
